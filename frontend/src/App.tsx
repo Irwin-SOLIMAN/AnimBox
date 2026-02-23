3e53387cfe80ebaddda1e7529aa7e82697352051
Irwin <irwin.soliman@gmail.com>
2026-02-23 10:50:27 +0100

    feat(frontend): init React + Tailwind + Router + Zustand

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
